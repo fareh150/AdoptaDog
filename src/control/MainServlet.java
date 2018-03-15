1b68555290ac161488f298909d669f59b8ef1814
admin <admin@pwc1>
2018-03-15 15:56:08 +0100

    Ana arreeglo

--- src/control/MainServlet.java
+++ src/control/MainServlet.java
@@ -2,11 +2,9 @@ package control;
 
 public class MainServlet {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Hola Juan");
-=======
-		System.out.println("jaja");
->>>>>>> branch 'master' of https://github.com/fareh150/AdoptaDog.git
+
 	}
 
 }
