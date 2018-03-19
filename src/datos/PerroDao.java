a909973851bc94c76e3a4cbcedaa78c4b7c08859
admin <admin@pwc1>
2018-03-19 14:48:56 +0100

    Creada PerroDao

--- src/datos/PerroDao.java
+++ src/datos/PerroDao.java
@@ -1,5 +1,29 @@
 package datos;
 
-public class PerroDao {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Perro;
+import utilidades.Conexion;
+
+public class PerroDao {
+	public List<Perro> listarPerro(){
+		List<Perro> perros= new ArrayList<>();
+		Conexion con = new Conexion();
+    	Connection conexion = con.conectar();
+    	String query = "SELECT * FROM PERRO";
+    	ResultSet rs = con.consultaSQL(conexion, query);
+    	try {
+    		while (rs.next()) {
+    			Perro perrito = new Perro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+        		perros.add(perrito);    			
+    		}
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+    	return perros;
+	}
 }
