37b5f4ee60c421988db6187674ac0945c5f6119c
admin <admin@pwc1>
2018-03-20 16:02:44 +0100

    Arreglos del proyecto

--- src/control/PerroServlet.java
+++ src/control/PerroServlet.java
@@ -1,58 +1,41 @@
 package control;
 
 import java.io.IOException;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import datos.PerroDao;
-import model.Perro;
-
 /**
- * Servlet implementation class MainServlet
+ * Servlet implementation class PerroServlet
  */
-//@WebServlet("/MainServlet")
-public class MainServlet extends HttpServlet {
+@WebServlet("/PerroServlet")
+public class PerroServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	public MainServlet() {
-		super();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PerroServlet() {
+        super();
+        // TODO Auto-generated constructor stub
     }
-
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
-		// TODO Auto-generated method stub
-		
-		PerroDao perroDao = new PerroDao();
-		List<Perro> listPerros = perroDao.listarPerro();
-		
-		request.setAttribute("listaPerros", listPerros);
-		RequestDispatcher view = request.getRequestDispatcher("Principal.jsp");
-        view.forward(request, response);
-
-	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		 processRequest(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		processRequest(request, response);
+		doGet(request, response);
 	}
 
 }
