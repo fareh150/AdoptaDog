d764b9f94ccf6eda9911482c51d2c3e427f5f2a2
Ana Solorzano <solorzanoa16@gmail.com>
2018-03-21 18:41:04 +0100

    Se actualizan los atributos de la clase perro segun como estan BBDD.
    Se crea metodo DetallePerro en la clase PerroDao.

--- src/datos/PerroDao.java
+++ src/datos/PerroDao.java
@@ -5,33 +5,35 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import model.Perro;
 import utilidades.Conexion;
+
 /**
  * Se realiza la busqueda de todos los perros en BBDD
+ * 
  * @author DavidClaro
  *
  */
 public class PerroDao {
-	public List<Perro> listarPerro(){
-		
-		List<Perro> perros= new ArrayList<>();
+	public List<Perro> listarPerro() {
+
+		List<Perro> perros = new ArrayList<>();
 		Conexion con = new Conexion();
-    	Connection conexion = con.conectar();
-    	String query = "SELECT * FROM PERRO LIMIT 6";
-    	ResultSet rs = con.consultaSQL(conexion, query);
-    	try {
-    		while (rs.next()) {
-    			Perro perrito = new Perro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
-        		perros.add(perrito);    			
-    		}
+		Connection conexion = con.conectar();
+		String query = "SELECT * FROM PERRO LIMIT 6";
+		ResultSet rs = con.consultaSQL(conexion, query);
+		try {
+			while (rs.next()) {
+				Perro perrito = new Perro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
+						rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9));
+				perros.add(perrito);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-    	return perros;
+		return perros;
 	}
-	
+
 	/**
 	 * Realiza la consulta de perros por los siguientes campos:
 	 * <ul>
@@ -39,53 +41,83 @@ public class PerroDao {
 	 * <li>tamanno</li>
 	 * <li>genero</li>
 	 * </ul>
+	 * 
 	 * @param edad
 	 * @param tamanno
 	 * @param genero
 	 * @return
 	 * @author AnaSolorzano
 	 */
-	public List<Perro> buscarPerros(String edad, String tamanno, String genero)
-	{
+	public List<Perro> buscarPerros(String edad, String tamanno, String genero) {
 		Conexion con = new Conexion();
-    	Connection conexion = con.conectar();
-    	
-    	//Sentencia SQL principal
-    	StringBuffer sql =  new StringBuffer("SELECT * FROM PERRO WHERE 1=1");
-    	
-    	//Agregar filtros.
-    	agregarFiltro(edad, " AND EDAD = ", sql);
-    	agregarFiltro(genero, " AND GENERO = ", sql);
-    	agregarFiltro(tamanno, " AND TAMANNO = ", sql);
+		Connection conexion = con.conectar();
 
-    	//Se ejecuta la sentencia SQL y devuelve los resultados en un objeto ResultSet
-    	ResultSet rs = con.consultaSQL(conexion, sql.toString());
-    	
-    	List<Perro> perros= new ArrayList<>();
-    	
-    	try {
-    		while (rs.next()) {
-    			Perro perrito = new Perro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
-        		perros.add(perrito);    			
-    		}
+		// Sentencia SQL principal
+		StringBuffer sql = new StringBuffer("SELECT * FROM PERRO WHERE 1=1");
+
+		// Agregar filtros.
+		agregarFiltro(edad, " AND EDAD = ", sql);
+		agregarFiltro(genero, " AND GENERO = ", sql);
+		agregarFiltro(tamanno, " AND TAMANNO = ", sql);
+
+		// Se ejecuta la sentencia SQL y devuelve los resultados en un objeto
+		// ResultSet
+		ResultSet rs = con.consultaSQL(conexion, sql.toString());
+
+		List<Perro> perros = new ArrayList<>();
+
+		try {
+			while (rs.next()) {
+				Perro perrito = new Perro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
+						rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9));
+				perros.add(perrito);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-    	return perros;
+		return perros;
 	}
-	
+
 	/**
 	 * Agrega los filtros de busqueda a la sentencia SQL principal.
+	 * 
 	 * @param campo
 	 * @param andSql
 	 * @param sql
 	 */
-	private void agregarFiltro(String campo , String andSql , StringBuffer sql)
-	{
-		if(campo != null && !campo.isEmpty())
-    	{
-			//sql.append(" AND GENERO = ").append("'").append("MACHO").append("'");
-    		sql.append(andSql).append("'").append(campo).append("'");
-    	}
+	private void agregarFiltro(String campo, String andSql, StringBuffer sql) {
+		if (campo != null && !campo.isEmpty()) {
+			// sql.append(" AND GENERO =
+			// ").append("'").append("MACHO").append("'");
+			sql.append(andSql).append("'").append(campo).append("'");
+		}
+	}
+	/**
+	 * Metodo que busca en BBDD toda la informacion del perro seleccionado
+	 * @param perroID
+	 * @return
+	 */
+	public Perro detallePerro(int perroID) {
+		Perro perrito = new Perro();
+		Conexion con = new Conexion();
+		Connection conexion = con.conectar();
+		String query = "SELECT * FROM PERRO WHERE PerroID=" + perroID;
+		ResultSet rs = con.consultaSQL(conexion, query);
+		try {
+			while (rs.next()) {
+				 perrito.setID(rs.getInt(1));
+				 perrito.setNombre(rs.getString(2));
+				 perrito.setEdad(rs.getString(3));				 
+				 perrito.setTamano(rs.getString(4));
+				 perrito.setGenero(rs.getString(5));
+				 perrito.setImagen(rs.getString(6));
+				 perrito.setRaza(rs.getString(7));
+				 perrito.setDescripcion(rs.getString(8));
+				 perrito.setEstado(rs.getString(9));				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return perrito;
 	}
 }
--- src/model/Perro.java
+++ src/model/Perro.java
@@ -2,45 +2,60 @@ package model;
 
 /**
  * La clase Perro, tiene sus atributos, que pueden ser modificados por el voluntario.
- * @author Ana.
+ * @author Ana Solorzano
  *
  */
 public class Perro {
 	
 	@Override
 	public String toString() {
-		return String.format("Perrito [ID=%s, nombre=%s, edad=%s, genero=%s, tamano=%s, imagen=%s]", ID, nombre, edad, genero, tamano, imagen);
+		return String.format("Perrito [ID=%s, nombre=%s, edad=%s, genero=%s, tamano=%s, imagen=%s, raza=%s, estado=%s, descripcion=%s]",
+				                         perroID, nombre, edad, genero, tamano, imagen, raza, estado, descripcion);
 	}
-	private int ID;
+	private int perroID;
 	private String nombre;
 	private String edad;
-	private String genero;
 	private String tamano;
+	private String genero;
 	private String raza;
 	private String imagen;
+	private String estado;
+	private String descripcion;
+	
+	public Perro(){
+		super();
+	}
 	
 	/**
+	 * @param iD
 	 * @param nombre
 	 * @param edad
-	 * @param genero
 	 * @param tamano
+	 * @param genero
+	 * @param raza
+	 * @param imagen
+	 * @param estado
+	 * @param descripcion
 	 */
-	public Perro(int ID, String nombre, String edad, String tamano, String genero, String raza, String imagen) {
+	public Perro(int iD, String nombre, String edad, String tamano, String genero, String raza, String imagen,
+			String estado, String descripcion) {
 		super();
-		this.ID = ID;
+		perroID = iD;
 		this.nombre = nombre;
 		this.edad = edad;
 		this.tamano = tamano;
 		this.genero = genero;
 		this.raza = raza;
 		this.imagen = imagen;
-	}
+		this.estado = estado;
+		this.descripcion = descripcion;
+	}		
 	
 	public int getID() {
-		return ID;
+		return perroID;
 	}
 	public void setID(int ID) {
-		this.ID = ID;
+		this.perroID = ID;
 	}
 	public String getNombre() {
 		return nombre;
@@ -72,6 +87,23 @@ public class Perro {
 	public void setImagen(String imagen) {
 		this.tamano = imagen;
 	}
-	
-	
+	public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
 }
